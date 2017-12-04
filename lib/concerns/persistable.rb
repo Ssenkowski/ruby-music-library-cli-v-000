278ed26a9d56029d3d8407f1da6f4fef9a702b8c
Steven Senkowski <scsenkowski@gmail.com>
2017-12-04 18:11:07 +0000

    automatically backed up by learn

--- lib/concerns/persistable.rb
+++ lib/concerns/persistable.rb
@@ -1,3 +1,21 @@
 module Concerns::Persistable
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(genre)
+    genre = self.new(genre)
+    @@all << genre
+    genre
+  end
   
 end
